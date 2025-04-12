6f2d413a73f909344e6a4dc35e0dda35f5e6d184
Aaditya Singh <72747190+Aadi16@users.noreply.github.com>
2025-04-13 03:26:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def load_models():
     for filename, file_id in model_files.items():
         file_path = os.path.join(model_dir, filename)
         if not os.path.exists(file_path):
-            st.write(f"Downloading {filename}...")
+            #st.write(f"Downloading {filename}...")
             url = f"https://drive.google.com/uc?id={file_id}"
             gdown.download(url, file_path, quiet=True)
 
